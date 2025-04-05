6af3610b615b5a3e5773383184b89e18d7863788
Flamezam <muazz24@student.sdu.dk>
2025-04-05 09:10:29 +0200

    finishing sicth linq

--- HW3 Data Visualization/ViewModels/BarChartViewModel.cs
+++ HW3 Data Visualization/ViewModels/BarChartViewModel.cs
@@ -1,49 +1,61 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Painting;
-using SkiaSharp;
 using System.Collections.Generic;
 using HW3_Data_Visualization.Models;
 using System.Linq;
 
 namespace HW3_Data_Visualization.ViewModels;
+
 public class BarChartViewModel : ChartViewModelBase
 {
-    // Make Title writable by adding a set accessor
     public override string Title { get; set; }
-
     public override IEnumerable<ISeries> SeriesCollection { get; }
-
     public override IEnumerable<Axis> XAxes { get; }
-
     public override IEnumerable<Axis> YAxes { get; }
 
     public BarChartViewModel(IEnumerable<FoodWasteData> data, string title = "Bar Chart")
     {
-        // Set the title from the constructor parameter
         Title = title;
 
-        // Initialize the series collection
+        // Determine which property to visualize based on the title
+        var values = data.Select(d =>
+            title.Contains("Economic Loss") ? d.EconomicLoss :
+            title.Contains("Per Capita") ? d.AvgWastePerCapita :
+            d.TotalWaste).ToArray();
+
+        // Use FoodCategory as label unless it's country-based data
+        var labels = data.Select(d =>
+            !string.IsNullOrWhiteSpace(d.FoodCategory) ? d.FoodCategory : d.Country).ToArray();
+
+        // Name based on chart type
+        var seriesName = title.Contains("Economic Loss") ? "Loss ($M)" :
+                         title.Contains("Per Capita") ? "Kg/Capita" :
+                         "Waste";
+
         SeriesCollection = new[]
         {
-                new ColumnSeries<double>
-                {
-                    Values = data.Select(d => d.TotalWaste).ToArray(),
-                    Name = "Waste"
-                }
-            };
+            new ColumnSeries<double>
+            {
+                Values = values,
+                Name = seriesName
+            }
+        };
 
-        // Initialize the X-axis
         XAxes = new[]
         {
-                new Axis { Labels = data.Select(d => d.FoodCategory).ToArray() }
-            };
+            new Axis { Labels = labels }
+        };
 
-        // Initialize the Y-axis
         YAxes = new[]
         {
-                new Axis { Labeler = value => $"{value:N0} Tons" }
-            };
+            new Axis
+            {
+                Labeler = value =>
+                    title.Contains("Economic Loss") ? $"${value:N1}M" :
+                    title.Contains("Per Capita") ? $"{value:N1} Kg" :
+                    $"{value:N0} Tons"
+            }
+        };
     }
 }
